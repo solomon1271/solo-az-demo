29fe87624b7d20897a34fdb934a742aeaa28fcba
sorael11@gmail.com <sorael11@gmail.com>
2024-10-30 12:10:10 -0500

    check in configuration change

--- solo-az-demo/Pages/Index.cshtml.cs
+++ solo-az-demo/Pages/Index.cshtml.cs
@@ -6,15 +6,17 @@ namespace solo_az_demo.Pages
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        private IConfiguration _configuration;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public IndexModel(ILogger<IndexModel> logger, IConfiguration configuration)
         {
             _logger = logger;
+            _configuration = configuration;
         }
 
         public void OnGet()
         {
-
+            ViewData["Greetings"] = _configuration["Greeting"];
         }
     }
 }
